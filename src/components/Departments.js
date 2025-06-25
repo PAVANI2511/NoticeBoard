65187f791b16ebcba0d7043e049f290b15b61fc4
Jyothika <yellarubailujyothika2005@gmail.com>
2025-06-25 11:45:04 +0530

    uploaded files

---
+++ src/components/Departments.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Departments.css';
+
+const Departments = () => {
+  const departments = [
+    "Computer Science & Engineering (CSE)",
+    "Computer Science & Engineering AI & ML (CSM)",
+    "Computer Science & Engineering Artificial Intelligence (CAI)",
+    "Computer Science & Engineering Data Science (CSD)",
+    "Computer Science & Engineering Cyber Security (CSC)",
+    "Electronics And Communication & Engineering (ECE)",
+    "Electrical And Electronics Engineering (EEE)",
+    "Mechanical Engineering (MEC)",
+    "Civil Engineering (CIV)"
+  ];
+
+  const handleClick = (dept) => {
+    alert(`You selected: ${dept}`);
+  };
+  return (
+    <div className="container">
+      <div className="header">Departments</div>
+      <div className="buttons">
+        {departments.map((dept, index) => (
+          <button
+            key={index}
+            className="dept-button"
+            onClick={() => handleClick(dept)}
+          >
+            {dept}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Departments;
